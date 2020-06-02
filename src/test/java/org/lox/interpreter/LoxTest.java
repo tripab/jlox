b7ca7a9f359dd9aed3db8decf283e40793fafc3c
Abhinav Tripathi <abhinav@Abhinavs-MacBook-Air-2.local>
2020-06-03 00:23:46 +0530

    code for section 10.6

--- src/test/java/org/lox/interpreter/LoxTest.java
+++ src/test/java/org/lox/interpreter/LoxTest.java
@@ -12,25 +12,41 @@ public class LoxTest {
     @Test
     public void testBlockAndScopes() throws IOException {
         // TODO: capture the output of the Lox script, so that we can assert its correctness
+        System.out.println("testBlockAndScopes");
         Lox.main(new String[]{"src/test/resources/scopes.lox"});
-    }
-
-    @Test
-    public void testForLoopOnFibonnaciSequence() throws IOException {
-        // TODO: capture the output of the Lox script, so that we can assert its correctness
-        Lox.main(new String[]{"src/test/resources/fibonacci.lox"});
-    }
-
-    @Test
-    public void testRecursiveFibonnaci() throws IOException {
-        // TODO: capture the output of the Lox script, so that we can assert its correctness
-        Lox.main(new String[]{"src/test/resources/recursive_fibonacci.lox"});
+        System.out.println("");
     }
 
     @Test
     public void testBasicFunction() throws IOException {
         // TODO: capture the output of the Lox script, so that we can assert its correctness
+        System.out.println("testBasicFunction");
         Lox.main(new String[]{"src/test/resources/sayHi.lox"});
+        System.out.println("");
+    }
+
+    @Test
+    public void testForLoopOnFibonnaciSequence() throws IOException {
+        // TODO: capture the output of the Lox script, so that we can assert its correctness
+        System.out.println("testForLoopOnFibonnaciSequence");
+        Lox.main(new String[]{"src/test/resources/fibonacci.lox"});
+        System.out.println("");
+    }
+
+    @Test
+    public void testRecursiveFibonnaci() throws IOException {
+        // TODO: capture the output of the Lox script, so that we can assert its correctness
+        System.out.println("testRecursiveFibonnacixs");
+        Lox.main(new String[]{"src/test/resources/recursive_fibonacci.lox"});
+        System.out.println("");
+    }
+
+    @Test
+    public void testBasicClosure() throws IOException {
+        // TODO: capture the output of the Lox script, so that we can assert its correctness
+        System.out.println("testBasicClosure");
+        Lox.main(new String[]{"src/test/resources/closures.lox"});
+        System.out.println("");
     }
 
 }
